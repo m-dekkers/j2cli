122ffa9b2e1544c888e43927a25198698deeab01
Martijn Dekkers <mdekkers@mesosphere.com>
2019-09-26 08:35:07 +0200

    Make PyInstaller work

--- j2cli/__init__.py
+++ j2cli/__init__.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python
 
 """ j2cli main file """
-from getversion import get_module_version
-from j2cli.cli import main
+import pkg_resources
 
 __author__ = "Mark Vartanyan"
 __email__ = "kolypto@gmail.com"
-__version__ = get_module_version(j2cli)
+__version__ = '0.3.12b'
 
+from j2cli.cli import main
 
 if __name__ == '__main__':
     main()
